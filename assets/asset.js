7eb8942fe0eb5c860317be92f10cd9569e009243
Muhammad Ridho <ridho3375@gmail.com>
2021-03-10 09:45:19 +0700

    commit landing page v1

---
+++ assets/asset.js
@@ -0,0 +1,29 @@
+var slideIndex = 0;
+showSlide();
+
+function showSlide() {
+  var i;
+  var slides = document.getElementsByClassName("slider");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlide, 2000); 
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
